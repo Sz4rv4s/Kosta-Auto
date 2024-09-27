7542b7f0d56c67048bfeaea47e48c02968b05dfe
Szarvas Péter <peti20031202@gmail.com>
2024-09-27 08:41:23 +0200

    initial commit: added backend

---
+++ partshop-backend/src/main/java/hu/kosztaauto/partshop/KosztaAutoPartShopApplication.java
@@ -0,0 +1,13 @@
+package hu.kosztaauto.partshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosztaAutoPartShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosztaAutoPartShopApplication.class, args);
+	}
+
+}
---
+++ partshop-backend/src/test/java/hu/kosztaauto/partshop/KosztaAutoPartShopApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.kosztaauto.partshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosztaAutoPartShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
