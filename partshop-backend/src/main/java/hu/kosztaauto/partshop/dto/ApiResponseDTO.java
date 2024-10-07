09980390d1265b65db27335d007d0e6f4651f261
Szarvas Péter <peti20031202@gmail.com>
2024-10-07 19:22:00 +0200

    feat: created a DTO for more extensive API responses

---
+++ partshop-backend/src/main/java/hu/kosztaauto/partshop/dto/ApiResponseDTO.java
@@ -0,0 +1,12 @@
+package hu.kosztaauto.partshop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiResponseDTO {
+    private String message;
+}
