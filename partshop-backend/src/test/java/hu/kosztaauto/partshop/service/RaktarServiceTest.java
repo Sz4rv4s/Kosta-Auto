9958c2b5b50a7b6c3c313ff0d2e0850aceb07a21
Koszta Péter <koszta.peti0702@gmail.com>
2024-10-16 12:23:58 +0200

    Made getAllItemsByWarehouse test

--- partshop-backend/src/test/java/hu/kosztaauto/partshop/service/RaktarServiceTest.java
+++ partshop-backend/src/test/java/hu/kosztaauto/partshop/service/RaktarServiceTest.java
@@ -53,7 +53,33 @@ public class RaktarServiceTest {
         Assertions.assertEquals( raktarList.size(), list.size() );
     }
 
-    @Test void testConvertToRaktarWithItemsDTOShouldConvertToRaktarWithItemsDTO() {
+    @Test
+    public void testGetAllItemsByWarehouseShouldReturnAllItemsByWarehouse() {
+        //given
+        Raktar r1 = new Raktar(
+                1L,"nev","varos","cim",15, List.of(
+                        new Alkatresz("cikkszam","megnevezes","tipus",1500)
+        )
+        );
+
+        Raktar r2 = new Raktar(
+                2L,"neve","varosa","cime",19, List.of(
+                new Alkatresz("cikkszama","megnevezese","tipusa",1590)
+        )
+        );
+
+        List<Raktar> raktarList = List.of(r1, r2);
+        Mockito.when( raktarRepository.findAll() ).thenReturn(raktarList);
+
+        //when
+        List<RaktarWithItemsDTO> lista = raktarService.getAllItemsByWarehouse();
+
+        //then
+        Assertions.assertEquals( raktarList.size(), lista.size() );
+    }
+
+    @Test
+    public void testConvertToRaktarWithItemsDTOShouldConvertToRaktarWithItemsDTO() {
         //given
 
         Alkatresz alkatresz = new Alkatresz(
