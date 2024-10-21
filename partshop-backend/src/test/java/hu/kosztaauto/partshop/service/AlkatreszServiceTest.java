3ba695224c20ffc788451227683c494d65efde76
Koszta Péter <koszta.peti0702@gmail.com>
2024-10-21 10:07:55 +0200

    Made convertToAlkatreszWithWarehouseDTO test

--- partshop-backend/src/test/java/hu/kosztaauto/partshop/service/AlkatreszServiceTest.java
+++ partshop-backend/src/test/java/hu/kosztaauto/partshop/service/AlkatreszServiceTest.java
@@ -2,6 +2,7 @@ package hu.kosztaauto.partshop.service;
 
 import hu.kosztaauto.partshop.dto.AlkatreszDTO;
 import hu.kosztaauto.partshop.dto.AlkatreszWithWarehouseDTO;
+import hu.kosztaauto.partshop.dto.RaktarDTO;
 import hu.kosztaauto.partshop.model.Alkatresz;
 import hu.kosztaauto.partshop.model.Raktar;
 import hu.kosztaauto.partshop.repository.AlkatreszRepository;
@@ -46,5 +47,39 @@ public class AlkatreszServiceTest {
         Assertions.assertEquals(alkatresz.getMegnevezes(), alkatreszDTO.getMegnevezes());
         Assertions.assertEquals(alkatresz.getAutoTipus(), alkatreszDTO.getAutoTipus());
         Assertions.assertEquals(alkatresz.getAr(), alkatreszDTO.getAr());
+    }
+
+    @Test
+    public void testConvertToAlkatreszWithWarehouseDTOShouldReturnAlkatreszWithWarehouseDTO() {
+        //given
+        Raktar raktar = new Raktar(
+                1L,"nev","varos","cim",15
+        );
+
+        Alkatresz alkatresz = new Alkatresz(
+                "cikkszam","megnevezes","autotipus",1000, raktar
+        );
+
+        RaktarDTO raktarDTO = new RaktarDTO(
+                alkatresz.getRaktar().getId(),
+                alkatresz.getRaktar().getNev(),
+                alkatresz.getRaktar().getVaros(),
+                alkatresz.getRaktar().getCim(),
+                alkatresz.getRaktar().getKapacitas()
+        );
+
+        //when
+        AlkatreszWithWarehouseDTO alkatreszWithWarehouseDTO = alkatreszService.convertToAlkatreszWithWarehouseDTO(alkatresz);
+
+        //then
+        Assertions.assertEquals(alkatresz.getCikkszam(), alkatreszWithWarehouseDTO.getCikkszam());
+        Assertions.assertEquals(alkatresz.getMegnevezes(), alkatreszWithWarehouseDTO.getMegnevezes());
+        Assertions.assertEquals(alkatresz.getAutoTipus(), alkatreszWithWarehouseDTO.getAutoTipus());
+        Assertions.assertEquals(alkatresz.getAr(), alkatreszWithWarehouseDTO.getAr());
+        Assertions.assertEquals(raktarDTO.getId(), alkatreszWithWarehouseDTO.getRaktar().getId());
+        Assertions.assertEquals(raktarDTO.getNev(), alkatreszWithWarehouseDTO.getRaktar().getNev());
+        Assertions.assertEquals(raktarDTO.getVaros(), alkatreszWithWarehouseDTO.getRaktar().getVaros());
+        Assertions.assertEquals(raktarDTO.getCim(), alkatreszWithWarehouseDTO.getRaktar().getCim());
+        Assertions.assertEquals(raktarDTO.getKapacitas(), alkatreszWithWarehouseDTO.getRaktar().getKapacitas());
     }
 }
