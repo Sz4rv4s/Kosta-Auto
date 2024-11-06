52a5128caeabf24506dc4ff936fbc5171203b408
Koszta Péter <koszta.peti0702@gmail.com>
2024-11-06 12:40:05 +0100

    Unused import deleted

--- partshop-backend/src/main/java/hu/kosztaauto/partshop/KostaAutoPartShopApplication.java
+++ partshop-backend/src/main/java/hu/kosztaauto/partshop/KostaAutoPartShopApplication.java
@@ -2,9 +2,6 @@ package hu.kosztaauto.partshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Entry point for the Kosta Auto Part Shop application.
